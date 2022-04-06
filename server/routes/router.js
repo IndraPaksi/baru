8875fcee91383f95876f00555b622df0c5899b17
IndraPaksi <101627667+IndraPaksi@users.noreply.github.com>
2022-04-06 21:54:32 +0700

    Add files via upload

---
+++ server/routes/router.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const route = express.Router()
+
+const services = require('../services/render');
+const controller = require('../controller/controller');
+
+route.get('/', services.homeRoutes);
+
+route.get('/add-user', services.add_user);
+
+route.get('/update-user',services.update_user);
+
+route.get('/view-user',services.view_user);
+
+route.post('/api/users', controller.create);
+route.get('/api/users', controller.find);
+route.get('/api/users/:id',controller.findById);
+route.post('/api/users/:id',controller.update);
+route.delete('/api/users/:id',controller.delete);
+
+module.exports = route
